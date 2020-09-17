f7e95af0c8f2fb02a25e00226c9f8fd41fe7b043
Mr.huang <2316694914@qq.com>
2020-09-17 17:56:47 +0800

    docs: 测试代码提交

--- test/index.js
+++ test/index.js
@@ -5,69 +5,127 @@
  *     this.next = null;
  * }
  */
+
+function ListNode(val) {
+    this.val = val;
+    this.next = null;
+}
+let listA = new ListNode(1),
+    la = listA;
+
+    la.next=new ListNode(2);
+    la = la.next;
+    la.next=new ListNode(3);
+    la = la.next;
+    la.next=new ListNode(4);
+    la = la.next;
+    la.next=new ListNode(5);
+
+//一开始做的时候 内存溢出了 发现有移动20万次
+//然后就想到了用k%链表长度来取得要实际移动的次数
+//后面的就其实没啥难度了
+//方法1：其实就是每次拿链表的最后一位放到头上 放 newk次 就是移动的次数
+//方法2: 就是利用哨兵 在19.从列表末尾删除第n个节点里面用到过
+//方法3: 移动到 倒数k+1步 也就是 length-k-1的位置
+//1-2-3-4-5 走2步(5-2-1)后的链表是  3-4-5
 /**
  * @param {ListNode} head
- * @param {number} n
+ * @param {number} k
  * @return {ListNode}
  */
-var removeNthFromEnd = function(head, n) {
-    let len =0,index=0,list = head;
+var rotateRight1 = function(head, k) {
+    if (!head || !head.next || k==0) return head;
+    //这里就是计算链表长度 和计算实际移动的次数
+    let h = head,length=0;
 
-    //遍历链表先获取长度
-    while (list){
-        list = list.next;
-        len++;
+    while (h){
+        h = h.next;
+        length++;
     }
-    //如果 len  等于 n 说明需要删除第一个节点 直接进行删除返回即可
-    if (len == n) {
-        head = head.next;
-        return head;
+    k = k%length;
+    //如果实际 k 和链表长度一致k就会等于0说明转了个圈 直接返回
+    if (k==0) return head;
+    h = head;
+    //走到倒数第二个节点停止
+    while (h.next.next){
+        h = h.next;
     }
-    list = head;
-    //长度减去n是要删除的节点 选中要删除的前一个元素 len-n-1 删除节点的前一个节点
-    while (len-n-1 != index){
-        list = list.next;
-        index++;
+    let tem = h.next;
+
+    //断尾
+    h.next = null;
+    //接头
+    tem.next = head;
+    head = tem;
+    k--;
+    return k > 0 ? rotateRight1(head,k) : head;
+};
+var rotateRight = function(head, k) {
+    if (!head || !head.next || k==0) return head;
+    let h = head,length=0;
+
+    while (h){
+        h = h.next;
+        length++;
     }
-    //删除节点
-    //list.next 是要删除的节点 所以list.next被list.next.next覆盖就是删除了该节点
-    list.next = list.next.next || null;
-    return head;
+    k = k%length;
+    if (k==0) return head;
+    //用哨兵的方式
+    // let h2 = head;
+
+    // h = head;
+    // while (h){
+    //     if (k<0) h2 = h2.next;
+    //     h = h.next;
+    //     k--;
+    // }
+    //length-k是新头的节点length-k-1为尾
+    k = length-k-1;
+    h = head;
+    while (k>0){
+        h = h.next;
+        k--;
+    }
+    //此时这里是length-k-1节点
+    let tem = h.next,
+        t =tem;
+
+    //断尾
+    h.next = null;
+
+    while (t.next){
+        t = t.next;
+   }
+   //接头
+   t.next = head;
+   head = tem;
+   return head;
+};
+//官方的方式更加精髓一下 先形成闭环再进行切断
+var rotateRight12 = function(head, k) {
+    if (!head || !head.next || k==0) return head;
+    //为了计算正确的长度 所以length起点1 因为判断点是h.next
+    let h = head,length=1;//length=0
+
+    while (h.next){
+        h = h.next;
+        length++;
+    }
+    //length++;
+    k = k%length;
+    if (k==0) return head;
+    //让这里形成环
+    h.next = head;
+    k = length-k;//倒数1步 走4步
+    //移动k步
+    while (k>0){
+        h = h.next;
+        k--;
+    }
+    let newHead = h.next;
+
+    h.next = null;
+   return newHead;
 };
 
-/**
- * Definition for singly-linked list.
- * function ListNode(val) {
- *     this.val = val;
- *     this.next = null;
- * }
- */
-/**
- * @param {ListNode} head
- * @param {number} n
- * @return {ListNode}
- */
-var removeNthFromEnd1 = function(head, n) {
-    let first = head,second = head;
-
-    // first链表 从开头移动到结尾并且n自减
-    // first linked list moves from the beginning to the end and n--
-    //当 n=0时，second链表开始移动节点
-    // When n=0,second linked list start moveing node
-    while (first){
-        if (n<0) second = second.next;
-        first = first.next;
-        n--;
-    }
-    //当first链表节点移动结束时
-    //When first linked list node move to the end
-    //当n等于0时，表示second链表节点没有移动，所以要删除的是第一个节点
-    // When n ==0,it means second linked list node has not moved,so the first node is to be deleted
-    if (n==0) return (head = second.next) && head;
-    //n小于0时,second链表节点的位置正好在要删除的前一个节点上
-    //When n < 0, second linked list node is located exactly on the previous node tobe deleted,
-    //因为second链表节点和fast链表节点的间隔为n
-    //Because second linked list node and fasr linked list node are spaced n apart
-    second.next = second.next.next || null;
-    return head;
-};
+console.log(rotateRight(listA,1));
--- test/index.py
+++ test/index.py
@@ -1,33 +1,94 @@
-#!/usr/bin/env python
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution:
-    def removeNthFromEnd(self, head, n):
-        length,index,listHead = 0,0,head
-        while listHead:
-            listHead = listHead.next
-            length +=1
-        if length == n:
-            head = head.next
-            return head
-        listHead = head
-        while length-n-1 != index:
-            listHead = listHead.next
-            index+=1
-        listHead = listHead.next.next or None
-        return head
-    def removeNthFromEnd1(self, head, n):
-        listA,listB = head,head
-        while listA:
-            if n <0 : listB = listB.next
-            listA = listA.next
-            n-=1
-        if n == 0: 
-            head = head.next
-            return head
-        listB = listB.next.next or None
-        return head
+"""
+# Definition for a Node.
 
+"""
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+
+class Solution:
+    def copyRandomList(self, head: 'Node') -> 'Node':
+        lista,newHead = head,Node(-1)
+
+        while lista:
+            tem = lista.next
+            node = Node(lista.val)
+            node.next = lista.next
+            lista.next = node
+            lista = tem
+        
+        lista = head
+
+        while lista:
+            if lista.random:
+                lista.next.random = lista.random.next
+            lista = lista.next.next
+
+        lista = head
+        node = newHead
+
+        while lista:
+            node.next = lista.next
+            node = node.next
+            lista.next = node.next
+            lista = lista.next
+        
+        return newHead.next
+    def copyRandomList(self, head: 'Node') -> 'Node':
+        lista,newHead = head,Node(-1)
+
+        while lista:
+            tem = lista.next
+            node = Node(lista.val)
+            node.next = lista.next
+            lista.next = node
+            lista = tem
+        
+        lista = head
+
+        while lista:
+            if lista.random:
+                lista.next.random = lista.random.next
+            lista = lista.next.next
+
+        lista = head
+        node = newHead
+
+        while lista:
+            node.next = lista.next
+            node = node.next
+            lista.next = node.next
+            lista = lista.next
+        
+        return newHead.next
+
+#生成题的链表
+listA = Node(7)
+la = listA
+i=0
+obj={}
+obj[i] = la
+i+=1
+la.next = Node(13)
+la = la.next
+obj[i] = la
+i+=1
+la.next = Node(11)
+la = la.next
+obj[i] = la
+i+=1
+la.next = Node(10)
+la = la.next
+obj[i] = la
+i+=1
+la.next = Node(1)
+la = la.next
+obj[i] = la
+i+=1
+obj[1].random = obj[0]
+obj[2].random = obj[4]
+obj[3].random = obj[2]
+obj[4].random = obj[0]
+print(listA == Solution().copyRandomList(listA))
