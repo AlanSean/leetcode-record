3bb7620dc4ddc0d79d6df1bc67af369ca215f971
Mr.huang <2316694914@qq.com>
2020-09-18 18:01:28 +0800

    docs: 了解二叉树

--- test/index.js
+++ test/index.js
@@ -1,131 +1,54 @@
 /**
- * Definition for singly-linked list.
- * function ListNode(val) {
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
  *     this.val = val;
- *     this.next = null;
+ *     this.left = this.right = null;
  * }
  */
-
-function ListNode(val) {
-    this.val = val;
-    this.next = null;
-}
-let listA = new ListNode(1),
-    la = listA;
-
-    la.next=new ListNode(2);
-    la = la.next;
-    la.next=new ListNode(3);
-    la = la.next;
-    la.next=new ListNode(4);
-    la = la.next;
-    la.next=new ListNode(5);
-
-//一开始做的时候 内存溢出了 发现有移动20万次
-//然后就想到了用k%链表长度来取得要实际移动的次数
-//后面的就其实没啥难度了
-//方法1：其实就是每次拿链表的最后一位放到头上 放 newk次 就是移动的次数
-//方法2: 就是利用哨兵 在19.从列表末尾删除第n个节点里面用到过
-//方法3: 移动到 倒数k+1步 也就是 length-k-1的位置
-//1-2-3-4-5 走2步(5-2-1)后的链表是  3-4-5
 /**
- * @param {ListNode} head
- * @param {number} k
- * @return {ListNode}
+ * @param {TreeNode} root
+ * @return {number[]}
  */
-var rotateRight1 = function(head, k) {
-    if (!head || !head.next || k==0) return head;
-    //这里就是计算链表长度 和计算实际移动的次数
-    let h = head,length=0;
-
-    while (h){
-        h = h.next;
-        length++;
+var preorderTraversal1 = function (root) {
+    function forwards(rootTree, arr) {
+        if (!rootTree) return rootTree;
+        arr.push(rootTree.val);
+        forwards(rootTree.left,arr);
+        forwards(rootTree.right,arr);
     }
-    k = k%length;
-    //如果实际 k 和链表长度一致k就会等于0说明转了个圈 直接返回
-    if (k==0) return head;
-    h = head;
-    //走到倒数第二个节点停止
-    while (h.next.next){
-        h = h.next;
-    }
-    let tem = h.next;
+    let a = [];
 
-    //断尾
-    h.next = null;
-    //接头
-    tem.next = head;
-    head = tem;
-    k--;
-    return k > 0 ? rotateRight1(head,k) : head;
+    forwards(root,a);
+    console.log(a);
 };
-var rotateRight = function(head, k) {
-    if (!head || !head.next || k==0) return head;
-    let h = head,length=0;
-
-    while (h){
-        h = h.next;
-        length++;
+var preorderTraversal2 = function (root) {
+    function forwards(rootTree, arr) {
+        if (!rootTree) return rootTree;
+        arr.push(rootTree.val);
+        forwards(rootTree.left,arr);
+        forwards(rootTree.right,arr);
     }
-    k = k%length;
-    if (k==0) return head;
-    //用哨兵的方式
-    // let h2 = head;
+    let a = [];
 
-    // h = head;
-    // while (h){
-    //     if (k<0) h2 = h2.next;
-    //     h = h.next;
-    //     k--;
-    // }
-    //length-k是新头的节点length-k-1为尾
-    k = length-k-1;
-    h = head;
-    while (k>0){
-        h = h.next;
-        k--;
-    }
-    //此时这里是length-k-1节点
-    let tem = h.next,
-        t =tem;
-
-    //断尾
-    h.next = null;
-
-    while (t.next){
-        t = t.next;
-   }
-   //接头
-   t.next = head;
-   head = tem;
-   return head;
-};
-//官方的方式更加精髓一下 先形成闭环再进行切断
-var rotateRight12 = function(head, k) {
-    if (!head || !head.next || k==0) return head;
-    //为了计算正确的长度 所以length起点1 因为判断点是h.next
-    let h = head,length=1;//length=0
-
-    while (h.next){
-        h = h.next;
-        length++;
-    }
-    //length++;
-    k = k%length;
-    if (k==0) return head;
-    //让这里形成环
-    h.next = head;
-    k = length-k;//倒数1步 走4步
-    //移动k步
-    while (k>0){
-        h = h.next;
-        k--;
-    }
-    let newHead = h.next;
-
-    h.next = null;
-   return newHead;
+    forwards(root,a);
+    console.log(a);
 };
 
-console.log(rotateRight(listA,1));
+function TreeNode(val) {
+    this.val = val;
+    this.left = this.right = null;
+}
+
+let root = new TreeNode('F'),
+    r = root;
+
+r.left = new TreeNode('B');
+r.left.left = new TreeNode('A');
+r.left.right = new TreeNode('D');
+r.left.right.left = new TreeNode('C');
+r.left.right.right = new TreeNode('E');
+r.right = new TreeNode('G');
+r.right.right = new TreeNode('I');
+r.right.right.left = new TreeNode('H');
+
+preorderTraversal(root);
--- test/index.py
+++ test/index.py
@@ -1,94 +1,44 @@
-"""
-# Definition for a Node.
-
-"""
-class Node:
-    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
-        self.val = int(x)
-        self.next = next
-        self.random = random
+#Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+    def keys(self):
+        return ['val', 'next']
+    def __getitem__(self, item):
+        return getattr(self, item)
 
 class Solution:
-    def copyRandomList(self, head: 'Node') -> 'Node':
-        lista,newHead = head,Node(-1)
+    def rotateRight(self, head: ListNode, k: int) -> ListNode:
+        if not head or not head.next or k==0: return head
+        oldHead,length = head,1
 
-        while lista:
-            tem = lista.next
-            node = Node(lista.val)
-            node.next = lista.next
-            lista.next = node
-            lista = tem
-        
-        lista = head
+        while oldHead.next:
+            oldHead = oldHead.next
+            length+=1
 
-        while lista:
-            if lista.random:
-                lista.next.random = lista.random.next
-            lista = lista.next.next
+        k = k%length
+        if k == 0:return head
 
-        lista = head
-        node = newHead
+        oldHead.next,k = head,length-k
 
-        while lista:
-            node.next = lista.next
-            node = node.next
-            lista.next = node.next
-            lista = lista.next
-        
-        return newHead.next
-    def copyRandomList(self, head: 'Node') -> 'Node':
-        lista,newHead = head,Node(-1)
+        while k>0:
+            oldHead = oldHead.next
+            k-=1
+        newHead = oldHead.next
+        oldHead.next = None
+        head = newHead
 
-        while lista:
-            tem = lista.next
-            node = Node(lista.val)
-            node.next = lista.next
-            lista.next = node
-            lista = tem
-        
-        lista = head
-
-        while lista:
-            if lista.random:
-                lista.next.random = lista.random.next
-            lista = lista.next.next
-
-        lista = head
-        node = newHead
-
-        while lista:
-            node.next = lista.next
-            node = node.next
-            lista.next = node.next
-            lista = lista.next
-        
-        return newHead.next
-
-#生成题的链表
-listA = Node(7)
+        return head
+listA = ListNode(1)
 la = listA
-i=0
-obj={}
-obj[i] = la
-i+=1
-la.next = Node(13)
+la.next = ListNode(2)
 la = la.next
-obj[i] = la
-i+=1
-la.next = Node(11)
+la.next = ListNode(3)
 la = la.next
-obj[i] = la
-i+=1
-la.next = Node(10)
+la.next = ListNode(4)
 la = la.next
-obj[i] = la
-i+=1
-la.next = Node(1)
-la = la.next
-obj[i] = la
-i+=1
-obj[1].random = obj[0]
-obj[2].random = obj[4]
-obj[3].random = obj[2]
-obj[4].random = obj[0]
-print(listA == Solution().copyRandomList(listA))
+la.next = ListNode(5)
+
+
+Solution().rotateRight(listA,2)
